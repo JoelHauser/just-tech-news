35b8a69fbfbcf52ad60a0f5358cfc6551ed86ffe
JoelHauser <joel.hauser@gmail.com>
2022-04-29 22:04:35 -0400

    update

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const res = require("express/lib/response");
+const { Comment } = require("../../models");
+const sequelize = require('../../config/connection');
+
+router.get("/", (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.post("/", (req, res) => {});
+
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;
